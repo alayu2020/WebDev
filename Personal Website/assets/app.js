9c717d69648a253f0cf6d414a87b40e73956234e
alayu2020 <alayu@umich.edu>
2017-11-21 11:10:10 -0500

    Added Personal Website

---
+++ Personal Website/assets/app.js
@@ -0,0 +1,41 @@
+menu = {};
+
+// ready event
+menu.ready = function() {
+
+  // selector cache
+  var
+    $menuItem = $('.menu a.item, .menu .link.item'),
+    // alias
+    handler = {
+      activate: function() {
+        $(this)
+        .addClass('active')
+        .closest('.ui.menu')
+        .find('.item')
+        .not($(this))
+        .removeClass('active');
+      }
+    }
+  ;
+
+  
+  $menuItem
+    .on('click', handler.activate)
+  ;
+};
+// attach ready event
+$(document).ready(function(){
+  var contentDisplay = $('.content');
+  
+  contentDisplay.click(function () {
+    
+  })
+  
+
+});
+
+$(document).ready(menu.ready);
+
+
+
