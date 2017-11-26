df0723733adf8cc57bf79dba7bca7e76d01b578b
alayu2020 <alayu@umich.edu>
2017-11-25 21:45:24 -0500

    Updated Personal Website

--- Personal Website/assets/app.js
+++ Personal Website/assets/app.js
@@ -9,33 +9,33 @@ menu.ready = function() {
     // alias
     handler = {
       activate: function() {
+        var activeTab = $('.menu > a.item.active > i').attr('href');
+        activeTab = "#" + activeTab;
+        console.log(activeTab);
+        $(activeTab).removeClass('active');
+        $(activeTab).addClass('tab-content hide');
+
         $(this)
         .addClass('active')
         .closest('.ui.menu')
         .find('.item')
         .not($(this))
         .removeClass('active');
+
+        var targetTab = $('.menu > a.item.active > i').attr('href');
+        targetTab = "#" + targetTab;
+        $(targetTab).removeClass('hide');
+        $(targetTab).addClass('active');
       }
     }
   ;
-
-  
   $menuItem
     .on('click', handler.activate)
   ;
+  
 };
 // attach ready event
-$(document).ready(function(){
-  var contentDisplay = $('.content');
-  
-  contentDisplay.click(function () {
-    
-  })
-  
-
-});
 
 $(document).ready(menu.ready);
-
 
 
